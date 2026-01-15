5000e32107e5c7aa7573aa34a369ce392260db4d
Ayu <hujiaoyan88@gmail.com>
2026-01-15 19:49:37 +0900

    kakuninn

--- Services/Controller.cs
+++ Services/Controller.cs
@@ -1,6 +1,33 @@
-﻿namespace WebApplication5.Services
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Services;
+
+namespace WebApplication5.Services
 {
-    public class Controller
+
+
+    [ApiController]
+    [Route("api/daily-mail")]
+    public class DailyMailController : ControllerBase
     {
+        private readonly EmailService _emailService;
+
+        public DailyMailController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send(
+     [FromHeader(Name = "X-CRON-SECRET")] string secret,
+     IConfiguration config)
+        {
+            if (secret != config["DailyMail:Secret"])
+                return Unauthorized();
+
+            await _emailService.SendDailyEmailsAsync();
+            return Ok();
+        }
+
     }
 }
+
